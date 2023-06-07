b641a8e9ff39e224b04ccf5e3b6e7d74f400c50a
Sameer Mirza <srmirza14@gmail.com>
2023-06-06 20:25:55 -0400

    added gameData & timeTable objs

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,13 +2,29 @@
 const timerDisplay = document.querySelector('#timer');
 const sellBtn = document.querySelector('#sell-btn');
 
-const time = 10;
+var gameData = {
+    money: 0,
+    shards: 0,
+    tokens: 0
+};
+
+var timeTable = {
+    int1: 10,
+    int2: 30,
+    int3: 60,
+    int4: 120
+}
+
+let time = 10;
 
 // Functions
 
 function timeLoop () {
     console.log('time loop started')
-    setInterval(timeLoop, 1000);
+
+    setInterval(time, 1000);
+
+    timerDisplay.textContent = time;
 
 };
 
